abefaf8daf8779b2bcaf8997535a7b96ba0af662
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-04-28 18:14:12 +0600

    "this" Keyword and Arrow Functions

--- 10_Arrow_Functions_and_this_Keyword.js
+++ 10_Arrow_Functions_and_this_Keyword.js
@@ -62,3 +62,110 @@ function introduce(language) {
 introduce('JavaScript');
 
 
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//:::::::::::::::::::::::::"this" and Arrow Functions::::::::::::::::::::::::::::::::::
+
+/*With regular functions, the value of 'this' is set based on how the function is 
+called. With arrow functions, the value of 'this' is based on the function's 
+surrounding context. In other words, the value of 'this' inside an arrow function is 
+the same as the value of 'this' outside the function. */
+
+// constructor
+// function IceCream() {
+//     this.scoops = 0;
+// }
+
+// add scoop to ice cream
+// IceCream.prototype.addScoop = function() {
+//     setTimeout(function() { 
+//         this.scoops++;
+//         // console.log(this);
+//         console.log(`Scoop Added!`);
+//         // console.log(dessert.scoops);
+//     }, 2000);
+// };
+
+// const dessert = new IceCream();
+// console.log(dessert);
+// dessert.addScoop();
+// console.log(dessert.scoops);
+
+
+// One way around this is to use closure:
+// constructor
+// function IceCream() {
+//     this.scoops = 0;
+// }
+
+// add scoop to ice cream
+// IceCream.prototype.addScoop = function() {
+//     const cone = this;
+//     setTimeout(function() { 
+//         cone.scoops++;
+//         // console.log(this);
+//         console.log(`Scoop Added!`);
+//         console.log(dessert);
+//         console.log(dessert.scoops);      
+//     }, 0.5);
+// };
+
+// const dessert = new IceCream();
+// console.log(dessert);
+// dessert.addScoop();
+// console.log(dessert.scoops);
+// console.log(dessert);
+
+
+
+/*Well that's exactly what arrow functions do, so let's replace the function passed to setTimeout() with an arrow function: */
+
+// constructor
+// function IceCream() {
+//     this.scoops = 0;
+// }
+
+// add scoop to ice cream
+// IceCream.prototype.addScoop = function() {
+//     setTimeout( () => {    // an arrow function is passed to setTimeout
+//         this.scoops++;
+//         console.log(this);
+//         console.log(`Scoop Added!`);
+//         console.log(dessert);
+//         console.log(dessert.scoops);
+//     }, 2000);
+// };
+
+// const dessert = new IceCream();
+// console.log(dessert);
+// dessert.addScoop();
+// dessert.addScoop();
+
+
+/*Now what would happen if we changed the addScoop() method to an arrow function? */
+// constructor
+function IceCream() {
+    this.scoops = 0;
+}
+
+// add scoop to ice cream
+IceCream.prototype.addScoop = () => {  // addScoop is now an arrow function
+    setTimeout( () => {
+        this.scoops++;
+        console.log(this);
+        console.log(`Scoop Added!`);
+        console.log(dessert);
+        console.log(dessert.scoops);
+    }, 2000);
+};
+
+const dessert = new IceCream();
+console.log(dessert);
+dessert.addScoop();
+
+/***Yeah, this doesn't work for the same reason - arrow functions inherit their 'this'
+value from their surrounding context. Outside of the addScoop() method, the value of
+'this' is the global object. So if addScoop() is an arrow function, the value of
+'this' inside addScoop() is the global object. Which then makes the value of 'this' in
+the function passed to setTimeout() also set to the global object! */
+
