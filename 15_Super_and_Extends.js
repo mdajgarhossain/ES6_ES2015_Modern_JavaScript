a393c30ee1b725bf48cf54648e8f7d974015a74f
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-05-14 16:50:36 +0600

    Super & Extends Keywords OR Superclass & Subclasses are all in action

--- 15_Super_and_Extends.js
+++ 15_Super_and_Extends.js
@@ -90,3 +90,90 @@ console.log(myMaple);
 myMaple.changeSeason('spring');
 console.log(myMaple);
 
+//:::ES6 Examples:::
+
+class Courses {
+    constructor(name, prerequisites = ['ES5'], duration = 7) {
+        this.name = name;
+        this.prerequisites = prerequisites;
+        this.duration = duration;
+        this.language = 'JavaScript';
+    }
+
+    courseStart() {
+        console.log(`Programming Language : ${this.language},
+Course Name: ${this.name},
+Must be finished within ${this. duration} Hours.`);
+    }
+
+    deadline() {
+        console.log(`'In Sha Allah', I will finish within ${this. duration} Hours.`); 
+    }
+}
+
+// const ES6 = new Courses('ES6', ['ES5', 'OO JavaScript'], 5);
+// console.log(ES6);
+// ES6.courseStart();
+
+class React extends Courses {
+    constructor(name, prerequisites, duration) {
+        super(name, prerequisites, duration);
+    }
+
+    processToStart() {
+        console.log(`Step 1: Crash Course with Project,
+Step 2: Documentation with Project Tutorial,
+Step 3: Video Tutorial with Project,
+Step 4: Develop Own Project.`);
+        super.deadline();
+    }
+}
+
+const react = new React('ReactJS', ['ES5', 'OO ES5', 'ES6'], 15);
+console.log(react);
+react.courseStart();
+react.processToStart();
+
+
+//:::another example:::
+
+// class Developers {
+//     constructor(name = 'Student', age = 18, language = 'JavaScript') {
+//         this.name = name;
+//         this.age = age;
+//         this.language = language;
+//         this.job = 'JavaScript Developer';
+//     }
+
+//     introduce() {
+//         console.log(`My name's ${this.name} and My favorite programming language is ${this.language}!`);
+//     }
+
+//     showAge() {
+//         console.log(`I am ${this.age} years old.`);
+//     }
+
+//     showJob() {
+//         console.log(`I am a ${this.job}.`);
+        
+//     }
+// }
+
+// class Coder extends Developers {
+//     constructor(contest, name, language) {
+//         super(name, language);
+//         this.contest = contest;
+//     }
+    
+
+//     showAge() {
+//         super.showAge();
+//         console.log(`I am from subclass's prototype`);
+//     }
+// }
+
+// const coder1 = new Coder(true, 'Ajgar');
+// console.log(coder1);
+// coder1.showAge();
+// coder1.showJob();
+
