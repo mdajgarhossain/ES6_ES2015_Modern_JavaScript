3acf1da86396d131a887deebed75412f7585872b
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-06-04 18:22:57 +0600

    Looping through Maps using MapIterator & for...of loop

---
+++ 25_Looping_Through_Maps.js
@@ -0,0 +1,84 @@
+//::::::::::::::::::::::::::::::::::Lesson 3- Built-ins :::::::::::::::::::::::::::::::
+//::::::::::::::::: Lecture 9- Looping Through Maps :::::::::::::::::::
+
+//:::Map Recap:::
+// const recapMap = new Map();
+
+// recapMap.set('Country Map', {name: 'Bangladesh', capital: 'Dhaka'});
+// recapMap.set('District');
+// recapMap.set('District', 'Cumilla');
+
+// recapMap.delete('District');
+
+// console.log(recapMap);
+
+// console.log(recapMap.has('District'));
+// console.log(recapMap.has('Country Map'));
+
+// console.log(recapMap.get('District'));
+// console.log(recapMap.get('Country Map'));
+
+// recapMap.clear();
+// console.log(recapMap);
+
+//:::::::::::::::Looping Through Maps::::::::::::::::::::::::::::::::
+
+//::::::::::::1. Using the MapIterator:::::::::::::::::
+const islam = new Map();
+
+islam.set('Base-1', 'Kalima');
+islam.set('Base-2', 'Namaz');
+islam.set('Base-3','Roza');
+islam.set('Base-4', 'Hazz');
+islam.set('Base-5', 'Zaqat');
+
+console.log(islam);
+
+const iteratorObjForKeys = islam.keys();
+console.log(iteratorObjForKeys.next());
+console.log(iteratorObjForKeys.next());
+console.log(iteratorObjForKeys.next());
+console.log(iteratorObjForKeys.next());
+console.log(iteratorObjForKeys.next());
+console.log(iteratorObjForKeys.next()); // iteration is over, done is true
+
+const iteratorObjForValues = islam.values();
+console.log(iteratorObjForValues.next());
+console.log(iteratorObjForValues.next());
+console.log(iteratorObjForValues.next()); // iteration is going on, done is false
+
+//::::::::::::::::2. Using a for...of Loop::::::::::::::::::
+const prayers = new Map();
+
+prayers.set('Number-1', 'Fajar');
+prayers.set('Number-2', 'Duhur');
+prayers.set('Number-3', 'Asor');
+prayers.set('Number-4', 'Magreeb');
+prayers.set('Number-5', 'Esha');
+
+console.log(prayers);
+
+for(const number of prayers) {
+    console.log(number);
+}
+
+for(const number of prayers) {
+    let [key, value] = number;
+    // console.log(key, value);
+    console.log(key);
+    console.log(value);
+}
+
+/*for(const number of prayers) {
+    for(index of number) {
+        if(index === number[0]) {
+           key = number[0];
+        }
+    }
+    for(index of number) {
+        if(index === number[1]) {
+            value = number[1];
+        }
+    }
+    console.log(key, value);
+}*/
